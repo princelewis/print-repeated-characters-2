1a0b40f489fa9a91da42e19b81102d9e4e261d3d
princelewis <princelewis4christ@yahoo.com>
2020-06-26 12:17:50 +0100

    Create a program to read strings and output duplicate characters and their corresponding values

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,26 @@
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        String word = scanner.nextLine();
+
+        List<String> wordArray = Arrays.stream(word.split(""))
+                .collect(Collectors.toList());
+
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for (String p : wordArray ) {
+            int count = Collections.frequency(wordArray, p);
+            if (count > 1) {
+                map.put(p,count);
+            }
+        }
+
+        map.forEach((k, v) -> System.out.println(k + " = " + v));
 
 
     }
